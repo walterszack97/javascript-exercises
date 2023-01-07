4b14de5eb362396c56862694d10050fd1442d9f5
zachary w <walterszack97@gmail.com>
2023-01-06 23:46:06 -0500

    Create function that returns user input a  specified amount of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    const myWord = string;
+    let myString = '';
+    
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (i = 1; i <= num; i++) {
+        myString += string;
+    }
+    return myString;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
