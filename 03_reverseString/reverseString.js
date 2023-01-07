bb9528e5f97ac8b5781426a4e72a79a54b90afd2
zachary w <walterszack97@gmail.com>
2023-01-06 23:58:08 -0500

    Create function to take in a string and return that string reversed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let myString = '';
 
+    for (let i = string.length - 1; i >= 0; i--) {
+        myString += string[i];
+    }
+
+    return myString;
 };
 
+reverseString('hello')
 // Do not edit below this line
 module.exports = reverseString;
