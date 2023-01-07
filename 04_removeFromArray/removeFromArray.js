4a8b6e36cf6bffba47fa52af5fceeb8b5df51484
zachary w <walterszack97@gmail.com>
2023-01-07 00:59:30 -0500

    Create function to filter out specified elements of an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
 
+const removeFromArray = function(array, ...remove) {
+    let newArray;
+    for (let i = 0; i < remove.length; i++)
+        for (let p = 0; p < array.length; p++) {
+            if (array[p] === remove[i]) {
+                array.splice(p, 1);
+            }
+        }
+
+    return newArray = array;
 };
+
+removeFromArray([1, 2, 3, 4], 3, 2, 4);
 
 // Do not edit below this line
 module.exports = removeFromArray;
