3442135d3c26768713b2f9dff16d809ab578f7f0
zachary w <walterszack97@gmail.com>
2023-01-22 02:09:20 -0500

    COmplete function and pass tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+
+    //create fibonacci array
+    let firsttNum = 1;
+    let nextNum = 1;
+    let fibArray = [];
+   
+    for (i = 1; i <= num; i++){
+        fibArray.push(firsttNum);
+        firsttNum = firsttNum + nextNum;
+        fibArray.push(nextNum)
+        nextNum = nextNum + firsttNum;
+    }
+
+    return fibArray[num - 1];
 
 };
+
+console.log(fibonacci(-1));
 
 // Do not edit below this line
 module.exports = fibonacci;
