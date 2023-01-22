bed2dacd1910490b3be7b340397fa12615343389
zachary w <walterszack97@gmail.com>
2023-01-22 01:19:55 -0500

    Complete function and pass tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    //remove punctuation
+    let myWord = word.toLowerCase().replace(/[^a-z]/g, "");
+    let wordLength = myWord.length;
+    for (let i = 0; i < wordLength; i++){
+        if (myWord[i] !== myWord[wordLength - 1]){
+            return false;
+        }
+        wordLength--;
+    }
+    return true;
 };
+
+const word = "racecar racecar"
+
+palindromes(word);
 
 // Do not edit below this line
 module.exports = palindromes;
