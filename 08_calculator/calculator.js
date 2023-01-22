893f2f3ab30bdae0a8a8ac832d9f40bdae147325
zachary w <walterszack97@gmail.com>
2023-01-21 23:29:13 -0500

    complete calculator.js functions and test exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((total, number) => {
+    return total + number;
+  }, 0)
 };
 
-const multiply = function() {
 
+
+const multiply = function(numbers) {
+  return numbers.reduce((total, number) => {
+    return total * number;
+  })
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0 || a === 1) return 1;
+  return a * factorial(a - 1);
 };
+
+const empty = 5;
+console.log(factorial(empty));
 
 // Do not edit below this line
 module.exports = {
